c62c4e119a15f473a56c64ff0d1ebdf4ed9b2a74
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-03 01:31:56 -0800

    Update Core.h

--- DrkEngine/src/Core.h
+++ DrkEngine/src/Core.h
@@ -15,57 +15,25 @@
 
 namespace Drk
 {
-    enum class LogType { INFO, WARN, ERROR }
+    enum class LogType { INFO, WARN, ERROR };
 
     class Logger
     {
     public:
-        static void init()
-        {
-            logtime = gmtime(nullptr);
-            fn = "logs/drk_engine"                + "_";
-            fn += std::to_string(logtime.tm_mon)  + ".";
-            fn += std::to_string(logtime.tm_mday) + ".";
-            fn += std::to_string(logtine.tm_year) + "_";
-            fn += std::to_string(logtime.tm_hour) + ":";
-            fn += std::to_string(logtine.tm_min)  + ":";
-            fn += std::to_string(logtime.tm_sec)  + ".";
-            fn += ".log";
-            
-            if (logfile.fopen(fn))
-                read = true;
-        }
-        
-        static void log(LogType type, std::string msg)
-        {
-            if (!ready)
-                init();
-            logfile << "[" << type_str(type) << "]  " << msg << std::endl;
-        }
-    
-        static std::string type_str(LogType type)
-        {
-            switch (type)
-            {
-            case INFO : return "Info   ";
-            case WARN : return "Warning";
-            case ERR  : return "Error  ";
-            default   : return "";
-            }
-        }
+        static void init(void);
+        static void log(LogType type, std::string msg);
     
     private:
         static std::ofstream logfile;
         static std::tm* logtime;
         static std::string fn;
         static bool ready;
+        
+        static std::string type_str(LogType type);
     };
-}
-
 
 #ifdef DRK_LOG
     #define LOG(type, msg) Drk::Logger::log(type, msg)
-    Drk::Logger::init();
 #else
     #define LOG(type, msg)
 #endif
@@ -73,18 +41,11 @@ namespace Drk
 
 ////////// Assert //////////
 
-namespace Drk
-{
     class Assert
     {
-        static void failed(const std::string& msg, const char* file, int line)
-        {
-            std::string log_msg("Assert: " + (std::string)file + " [" std::to_string(line) "] " + msg);
-            LOG(LogType::ERROR, log_msg);
-        }
+    public:
+        static void failed(const std::string& msg, const char* file, int line);
     }
-}
-
 
 #ifdef DRK_ASSERT
     #define ASSERT(cond, msg) \
@@ -98,6 +59,8 @@ namespace Drk
 #else
     #define ASSERT(cond, msg)
 #endif
+    
+}
 
 
 #endif // CORE_H
