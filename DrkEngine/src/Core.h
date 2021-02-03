0ffeeccb01795299a47d59b03f86aec2a9dc47f5
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-03 01:50:34 -0800

    Update Core.h

--- DrkEngine/src/Core.h
+++ DrkEngine/src/Core.h
@@ -7,14 +7,27 @@
 #include <string>
 #include <ctime>
 
+#include <memory>
 #include <cassert>
 #include <filesystem>
 
 
-////////// Logger //////////
-
 namespace Drk
 {
+    ////////// Refs //////////
+    
+    template <typename T>
+    using Ptr = std::shared_ptr<T>;
+    
+    template <typename T, typename ... Args
+    constexpr Ptr<T> make_ptr(Args&& args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+    
+
+    ////////// Logger //////////
+    
     enum class LogType { INFO, WARN, ERROR };
 
     class Logger
@@ -32,14 +45,14 @@ namespace Drk
         static std::string type_str(LogType type);
     };
 
-#ifdef DRK_LOG
-    #define LOG(type, msg) Drk::Logger::log(type, msg)
-#else
-    #define LOG(type, msg)
-#endif
+    #ifdef DRK_LOG
+        #define LOG(type, msg) Drk::Logger::log(type, msg)
+    #else
+        #define LOG(type, msg)
+    #endif
 
 
-////////// Assert //////////
+    ////////// Assert //////////
 
     class Assert
     {
@@ -47,18 +60,18 @@ namespace Drk
         static void failed(const std::string& msg, const char* file, int line);
     }
 
-#ifdef DRK_ASSERT
-    #define ASSERT(cond, msg) \
-    do { \
-        if (!(cond)) \
-        { \
-            Drk::Assert::failed(msg, std::filesystem::path(__FILE__).filename().string(), __LINE__); \
-            std::assert(0); \
-        } \
-    } while (false)
-#else
-    #define ASSERT(cond, msg)
-#endif
+    #ifdef DRK_ASSERT
+        #define ASSERT(cond, msg) \
+        do { \
+            if (!(cond)) \
+            { \
+                Drk::Assert::failed(msg, std::filesystem::path(__FILE__).filename().string(), __LINE__); \
+                std::assert(0); \
+            } \
+        } while (false)
+    #else
+        #define ASSERT(cond, msg)
+    #endif
     
 }
 
