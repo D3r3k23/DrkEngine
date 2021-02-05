4dd1fbb9ccfbb8ed989c841929a2c4c97f2e70ae
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-05 12:04:04 -0800

    Update Eval.h

--- DrkEngine/inc/Eval.h
+++ DrkEngine/inc/Eval.h
@@ -10,13 +10,27 @@
 
 namespace Drk::Engine
 {
+    struct EvalWeights
+    {
+        double bishop;
+        double rook;
+        
+        void set_opening(void);
+        void set_middlegame(void);
+        void set_endgame(void);
+    };
+    
+    
     class Eval
     {
     public:
+        void load_position(const Position& position);
+        
         Ptr<std::vector<Chess::Move>> evaluate(const Chess::Position& position);
 
     private:
-    
+        Position position;
+        EvalWeights weights;
     };
 }
 
