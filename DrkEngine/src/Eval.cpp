aa5bf57a7281a67810681ac1977eb13b0c161174
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-05 12:05:34 -0800

    Update Eval.cpp

--- DrkEngine/src/Eval.cpp
+++ DrkEngine/src/Eval.cpp
@@ -4,5 +4,23 @@
 
 namespace Drk::Engine
 {
+    void EvalWeights::set_opening(void)
+    {
+        bishop = 1.0;
+        rook = 1.0;
+    }
+    
+    void EvalWeights::set_middlegame(void)
+    {
+        bishop = 1.5;
+        rook = 1.0;
+    }
+    
+    void EvalWeights::set_endgame(void)
+    {
+        bishop = 1.0;
+        rook = 1.5;
+    }
+    
     
 }
