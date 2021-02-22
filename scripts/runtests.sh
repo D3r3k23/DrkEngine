1c51ae439fa8723229ea6ee6c2a45985877bd339
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-21 20:31:48 -0800

    Update runtests.sh

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -3,5 +3,5 @@ executable_path = "tests/build/bin/Tests"
 if [[ -x $executable_path ]]; then
     ./$executable_path
 else
-    echo "Tests executable does not exist."
+    echo "Test executable does not exist."
 fi
