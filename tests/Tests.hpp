5d04c8121d88d37f989b77b5031b532395999559
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-21 20:17:22 -0800

    Update Tests.hpp

--- tests/Tests.hpp
+++ tests/Tests.hpp
@@ -10,38 +10,36 @@
 #include <chrono>
 
 
+#define RUN_TEST(test) DrkTest::Util::run(DrkTest::test, #test)
+
+
 namespace DrkTest
 {
-    //////// Utilities ////////
-
-    using Time  = std::chrono::time_point<std::chrono::steady_clock>;
-    using Micro = std::chrono::microseconds;
-
-    Time now(void)
-        { return std::chrono::steady_clock::now(); }
-
-
-    #define RUN_TEST(test) DrkTest::run(DrkTest::test, #test)
-
-    void run(std::function<bool()> func, const char* name)
+    namespace Util
     {
-        std::cout << "Test: " << name << std::endl;
+        using TimePnt  = std::chrono::time_point<std::chrono::steady_clock>;
+        using MicroSec = std::chrono::microseconds;
 
-        Time start  = now();
-        bool passed = func();
-        Time end    = now();
+        TimePnt now(void)
+            { return std::chrono::steady_clock::now(); }
 
-        Micro elapsed = std::chrono::time_point_cast<Micro>(end  ).time_since_epoch()
-                      - std::chrono::time_point_cast<Micro>(start).time_since_epoch();
+        void run(std::function<bool()> func, const char* name)
+        {
+            TimePnt start = now();
+            bool passed   = func();
+            TimePnt end   = now();
 
-        std::cout << "Result:  " << (passed ? "Passed" : "Failed") << std::endl;
-        std::cout << "Profile: " << elapsed.count() << " us" << std::endl;
-        std::cout << std::endl;
+            MicroSec elapsed = std::chrono::time_point_cast<MicroSec>(end  ).time_since_epoch()
+                             - std::chrono::time_point_cast<MicroSec>(start).time_since_epoch();
+
+            std::cout << "Test:    " << name                           << std::endl;
+            std::cout << "Result:  " << (passed ? "Passed" : "Failed") << std::endl;
+            std::cout << "Profile: " << elapsed.count() << " us"       << std::endl;
+            std::cout << std::endl;
+        }
     }
-
-
-    //////// Test Cases ////////
-
+    
+    
     bool test_1()
     {
         for (int i = 0; i < 20000; i++);
