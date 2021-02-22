a99785647a097dcfd722ccd81407a686c5076c03
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-21 20:30:24 -0800

    Update buildtests.sh

--- scripts/buildtests.sh
+++ scripts/buildtests.sh
@@ -1,5 +1,5 @@
 build_dir = "tests/build"
 
 mkdir -p $build_dir
-cmake -BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -B $build_dir
+cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -B $build_dir
 cmake --build $build_dir
