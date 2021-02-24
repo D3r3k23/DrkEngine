39bfdba04f74eac40812f78f9b071080b4c6abd1
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-24 14:00:53 -0800

    Update Tests.cpp

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -7,6 +7,28 @@ using namespace Drk;
 
 namespace DrkTest
 {
+    namespace Util
+    {
+        using Clock    = std::chrono::steady_clock;
+        using TimePnt  = std::chrono::time_point<Clock>;
+        using MicroSec = std::chrono::microseconds;
+
+        void run(std::function<bool()> func, const char* name)
+        {
+            TimePnt start = Clock::now();
+            bool passed   = func();
+            TimePnt end   = Clock::now();
+
+            MicroSec elapsed = std::chrono::time_point_cast<MicroSec>(end  ).time_since_epoch()
+                             - std::chrono::time_point_cast<MicroSec>(start).time_since_epoch();
+
+            std::cout << "Test:    " << name                           << std::endl;
+            std::cout << "Result:  " << (passed ? "Passed" : "Failed") << std::endl;
+            std::cout << "Profile: " << elapsed.count() << " us"       << std::endl;
+            std::cout << std::endl;
+        }
+    }
+    
     bool test_1()
     {
         for (int i = 0; i < 20000; i++);
