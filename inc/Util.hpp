176db042666f0e8095c47f6904fc3cd721c0cee6
Derek Henderson <d3r3k523@gmail.com>
2021-03-03 01:10:17 -0800

    Added Random class

--- inc/Util.hpp
+++ inc/Util.hpp
@@ -3,8 +3,19 @@
 #define UTIL_HPP
 
 
+#include <cstdlib>
+#include <ctime>
+
+
 namespace Drk::Util
 {
+    class Random
+    {
+        static void init(void);
+        static int get_in_range(int lower, int upper);
+    };
+
+
     bool in_bounds(int rank, int file);
 }
 
--- src/Util.cpp
+++ src/Util.cpp
@@ -4,6 +4,16 @@
 
 namespace Drk::Util
 {
+    void Random::init(void)
+    {
+        std::srand(time(NULL));
+    }
+
+    int Random::get_in_range(int lower, int upper)
+    {
+        return (std::rand() % (upper - lower)) - lower;
+    }
+
     bool in_bounds(int rank, int file)
     {
         return (0 <= rank && rank <= 7)
