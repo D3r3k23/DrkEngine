ecba1c8f342f79fffaed1e4bec64a0d715618f13
Derek Henderson <d3r3k523@gmail.com>
2021-03-07 23:08:48 -0800

    Updated cmake and scripts

--- scripts/buildtests.sh
+++ scripts/buildtests.sh
@@ -2,5 +2,9 @@
 build_dir="tests/build"
 
 mkdir -p $build_dir
-cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -S . -B $build_dir
-cmake --build $build_dir
+cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -S . -B $build_dir &&
+cmake --build $build_dir &&
+
+if [[ -f .vscode/c_cpp_properties.json ]]; then
+    cp tests/build/compile_commands.json .vscode
+fi
--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,7 +2,7 @@
 working_dir="tests"
 executable_path="build/bin/Tests"
 
-if [[ -x "${working_dir}/${executable_path}" ]]; then
+if [[ -f "${working_dir}/${executable_path}" ]]; then
     ( cd $working_dir && ./$executable_path )
 else
     echo "Test executable does not exist."
