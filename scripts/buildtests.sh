59eb8fa56383e76dd7fc99afc29c7f6c6550412a
Derek Henderson <d3r3k523@gmail.com>
2021-03-09 19:51:06 -0800

    updated buildtests.sh

--- scripts/buildtests.sh
+++ scripts/buildtests.sh
@@ -2,9 +2,7 @@
 build_dir="tests/build"
 
 mkdir -p $build_dir
-cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -S . -B $build_dir &&
-cmake --build $build_dir &&
+cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -S . -B $build_dir
+cmake --build $build_dir
 
-if [[ -f .vscode/c_cpp_properties.json ]]; then
-    cp tests/build/compile_commands.json .vscode
-fi
+cp "${build_dir}/compile_commands.json" .vscode
