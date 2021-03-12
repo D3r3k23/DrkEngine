476a6043f5fa8a32666ab8ec5ebb3cf8226b7c36
Derek Henderson <d3r3k523@gmail.com>
2021-03-11 23:37:28 -0800

    Checked build

--- scripts/buildtests.sh
+++ scripts/buildtests.sh
@@ -2,7 +2,10 @@
 build_dir="tests/build"
 
 mkdir -p $build_dir
-cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -S . -B $build_dir &&
-cmake --build $build_dir &&
 
-cp "${build_dir}/compile_commands.json" .vscode
+cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -S . -B $build_dir && 
+    if [[ -d .vscode ]]; then
+        cp "${build_dir}/compile_commands.json" .vscode
+    fi
+
+cmake --build $build_dir
--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,7 +2,7 @@
 working_dir="tests"
 executable_path="build/bin/Tests"
 
-if [[ -f "${working_dir}/${executable_path}" ]]; then
+if [[ -f ${working_dir}/${executable_path} ]]; then
     cd $working_dir
     ./$executable_path
 else
